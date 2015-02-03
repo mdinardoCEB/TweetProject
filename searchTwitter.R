b7c3bb1ae86dd61cd79f2a2d1161eea0c84edcef
mdinardoCEB <mdinardo@executiveboard.com>
2015-02-03 17:38:30 -0500

    Adds tweets

--- searchTwitter.R
+++ searchTwitter.R
@@ -43,17 +43,17 @@ timelineToCSV <- function(query = sample(c("@ChaseSupport", "@BofA_Help", "@Ask_
   }
   
   # Create a placeholder for the file
-  #file<-NULL
+  file<-NULL
   
   # Check if tweets.csv exists
-  #if (file.exists(saveFile)){file<- read.csv(saveFile)}
+  if (file.exists(saveFile)){file<- read.csv(saveFile)}
   
   # Merge the data in the file with our new tweets
   df <- do.call("rbind", lapply(tweets, as.data.frame))
-  #df<-rbind(df,file)
+  df<-rbind(df,file)
   
   # Remove duplicates
-  #df <- df[!duplicated(df[c("id")]),]
+  df <- df[!duplicated(df[c("id")]),]
   
   # Save
   write.csv(df,file=saveFile,row.names=FALSE)
